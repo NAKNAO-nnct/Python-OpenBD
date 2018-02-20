898f1c2294f4c98e60ee9ef2bab0bfbf14d5b401
中野雄貴 <totoro.windows@outlook.com>
2018-02-20 15:22:10 +0900

    pathを分離(変数化)

--- openbd.py
+++ openbd.py
@@ -1,9 +1,14 @@
 import json, requests, os
 
+# PATH
+result_html = 'result.html'
+result_template = 'views/template.tpl'
+book_json = 'book.json'
+
 # result.htmlに吐き出す
 def out_html(book_data_list):
     temp_data = ''
-    with open('views/template.tpl', 'r') as r_file:
+    with open(result_template, 'r') as r_file:
         for line_data in r_file:
             temp_data += line_data
     data = temp_data.format(
@@ -13,12 +18,12 @@ def out_html(book_data_list):
         book_data_list[3],  # 出版社
         book_data_list[4],  # 発売日
         book_data_list[0])  # ISBN
-    file = open('result.html', 'w')
+    file = open(result_html, 'w')
     file.write(data)
 
 # 詳細データから必要事項を取得
 def getNeedData():
-    file = open('book.json', 'r')
+    file = open(book_json, 'r')
     dict_json = json.load(file)
     if dict_json[0] == None:
         print('ISBNが不正です')
@@ -36,7 +41,7 @@ def getNeedData():
 def getBookData(isbn):
     url = 'http://api.openbd.jp/v1/get?isbn={}&pretty'.format(isbn)
     response = (requests.get(url)).text
-    file = open('book.json', 'w')
+    file = open(book_json, 'w')
     file.write(response)
 
 if __name__ == '__main__':
